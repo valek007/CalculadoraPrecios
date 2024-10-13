540277174497242d94b7d9fe17905b7743acae93
Valek007 <coronadev.king@gmail.com>
2024-10-13 12:57:21 +0200

    Estructura del proyecto:
    - Paquetes y clases principales donde voy a trabajar

---
+++ CalculadoraPrecios/src/main/java/CalculadoraMain.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class CalculadoraMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ CalculadoraPrecios/src/main/java/config/PropertiesLoader.java
@@ -0,0 +1,5 @@
+package main.java.config;
+
+public class PropertiesLoader {
+
+}
